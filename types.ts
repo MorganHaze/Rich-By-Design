ec67ea04cfe5d15b3c9ec5942a113e914a970645
MorganHaze <richbydesignHQ@outlook.com>
2026-01-11 19:40:17 -0600

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,7 +2,23 @@ export enum MarketingContentType {
   SOCIAL_POST = 'SOCIAL_POST',
   EMAIL_NEWSLETTER = 'EMAIL_NEWSLETTER',
   BLOG_OUTLINE = 'BLOG_OUTLINE',
+  BLOG_FULL = 'BLOG_FULL',
   AD_COPY = 'AD_COPY'
+}
+
+export enum PostStatus {
+  DRAFT = 'DRAFT',
+  SCHEDULED = 'SCHEDULED',
+  POSTED = 'POSTED'
+}
+
+export interface ScheduledPost {
+  id: string;
+  type: MarketingContentType;
+  platform: string;
+  content: string;
+  scheduledTime: number;
+  status: PostStatus;
 }
 
 export interface MarketingResult {
